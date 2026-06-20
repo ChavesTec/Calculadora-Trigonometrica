cf1f9f667e6d7a877f22ae68ef877690087d9d08
Alejandro Chaves <chavesfallasa@gmail.com>
2026-06-20 17:24:15 -0600

    Add files via upload

---
+++ CalculadoraTrigonometrica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora.trigonometrica;
+
+
+public class CalculadoraTrigonometrica {
+
+    public static void main(String[] args) {
+        System.out.println("Escriba 1 para funciones trigonometricas normales");
+        System.out.println("Escriba 2 para funciones trigonometricas inversas");
+        System.out.println("Escriba 3 para funciones trigonometricas hiperbólicas");
+    }
+    
+}
